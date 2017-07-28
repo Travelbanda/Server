2d243579dba01b1f0735db65a43eac1f9383c34b
kobezzza <kobezzza@gmail.com>
2017-07-28 19:07:59 +0300

    Added config

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*!
+ * V4Fire Server Core
+ * https://github.com/V4Fire/Server
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Server/blob/master/LICENSE
+ */
+
+import baseConfig from '@v4fire/core/src/config';
+export default Object.assign({}, baseConfig);
